6e2c83c4aa53f28ccd63c841c952937b181d0057
kabornblack <shaibu.kabiru@email.com>
2025-07-09 07:15:26 +0300

    Restructure folders

--- frontend/vet_ambulance/src/components/Owners/OwnerModal.jsx
+++ frontend/vet_ambulance/src/components/Owners/OwnerModal.jsx
@@ -1,5 +1,4 @@
 import { Modal, Box, Typography, TextField, Button } from "@mui/material";
-// import { createOwner, updateOwner } from "../../services/ownerService";
 import { useState, useEffect } from "react";
 import { createOwner, updateOwner } from "../../services/ownersService";
 
@@ -74,83 +73,3 @@ const OwnerModal = ({ modalOpen, setModalOpen, selectedOwner }) => {
 };
 
 export default OwnerModal;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import { useEffect, useState } from "react";
-// import { createOwner, updateOwner } from "../../services/ownersService";
-// import { Box, Button, TextField, Typography } from "@mui/material";
-// import Modal from "@mui/material/Modal";
-
-// const OwnerModal = ({ modalOpen, setModalOpen, selectedOwner }) => {
-//   const isCreate = selectedOwner.id === undefined;
-//   const [formData, setFormData] = useState({ name: "", phone: "" });
-
-//   useEffect(() => {
-//     setFormData({
-//       name: selectedOwner.name || "",
-//       phone: selectedOwner.phone || "",
-//     });
-//   }, [selectedOwner, setModalOpen]);
-
-//   const style = {
-//     position: "absolute",
-//     top: "50%",
-//     left: "50%",
-//     bgcolor: "white",
-//     width: 300,
-//     height: 300,
-//     transform: "translate(-50%, -50% )",
-//     p: 3,
-//     borderRadius: 1,
-//     boxShadow: 24,
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       if (isCreate) {
-//         await createOwner(formData);
-//       } else {
-//         await updateOwner(selectedOwner.id, formData);
-//       }
-//       setModalOpen(false);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-//       <Box sx={style}>
-//         <Typography sx={{ mb: 2 }}>
-//           {isCreate ? "Creating new owner" : "Updating owner"}
-//         </Typography>
-
-//         <form onSubmit={handleSubmit}>
-//           <TextField
-//             label="name"
-//             value={formData.name}
-//             onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-//             fullWidth
-//             sx={{ mb: 1 }}
-//           />
-//           <TextField
-//             label="phone"
-//             value={formData.phone}
-//             onChange={(e) =>
-//               setFormData({ ...formData, phone: e.target.value })
-//             }
-//             fullWidth
-//             sx={{ mb: 1 }}
-//           />
-//           <Button type="submit" variant="contained">
-//             {isCreate ? "Create" : "Update"}
-//           </Button>
-//         </form>
-//       </Box>
-//     </Modal>
-//   );
-// };
-
-// export default OwnerModal;
--- frontend/vet_ambulance/src/components/Owners/Owners.jsx
+++ frontend/vet_ambulance/src/components/Owners/Owners.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import OwnerModal from "./OwnerModal";
 import { Button } from "@mui/material";
-
 import { deleteOwner, getOwners } from "../../services/ownersService";
 
 const Owners = () => {
@@ -94,59 +93,3 @@ const Owners = () => {
 };
 
 export default Owners;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import { useEffect, useState } from "react";
-// import { getOwners } from "../../services/ownersService";
-// import { DataGrid } from "@mui/x-data-grid";
-// import Button from "@mui/material/Button";
-// import OwnerModal from "./OwnerModal";
-
-// const Owners = () => {
-//   const [owners, setOwners] = useState([]);
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [selectedOwner, setSelectedOwner] = useState({});
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const data = await getOwners();
-//         setOwners(data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   const columns = [
-//     { field: "id", headerName: "ID", width: 50 },
-//     { field: "name", headerName: "Name", width: 150 },
-//     { field: "phone", headerName: "Phone", width: 150 },
-//   ];
-
-//   // console.log(owners);
-
-//   const createOwner = () => {
-//     setSelectedOwner({});
-//     setModalOpen(true);
-//   };
-
-//   return (
-//     <>
-//       <h1>Owners</h1>
-//       <Button variant="contained" onClick={createOwner}>
-//         Add owner
-//       </Button>
-//       <DataGrid rows={owners} columns={columns}></DataGrid>
-//       <OwnerModal
-//         modalOpen={modalOpen}
-//         setModalOpen={setModalOpen}
-//         selectedOwner={selectedOwner}
-//       />
-//     </>
-//   );
-// };
-
-// export default Owners;
